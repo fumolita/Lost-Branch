db6dbb339a1aaa7c5d54c3942fb6bdf40981b359
c0uncer <burakcavdur2004@gmail.com>
2023-02-04 11:28:48 +0300

    sling_ilk

---
+++ BeikozJAM/Assets/Scripts/Sling.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sling : MonoBehaviour
+{
+    private bool isPressed;
+    private bool onCollision;
+
+    private float releaseDelay;
+
+    private Rigidbody2D rb;
+    private SpringJoint2D sj;
+    //public Rigidbody2D replica;
+
+    public GameObject sling;
+    private GameObject slingCircle;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sj = GetComponent<SpringJoint2D>();
+
+        releaseDelay = 1 / (sj.frequency * 4);
+    }
+    private void Start()
+    {
+        slingCircle = Instantiate(sling);
+        slingCircle.transform.position = transform.position;
+    }
+    private void DragBall()
+    {
+
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        rb.position = mousePosition;
+        //replica.position = mousePosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPressed)
+        {
+            DragBall();
+        }
+    }
+
+    private void OnMouseDown()
+    {
+            isPressed = true;
+            rb.isKinematic = true;
+            StopCoroutine(Release());
+            sj.enabled = true;
+    }
+    private void OnMouseUp()
+    {
+            isPressed = false;
+            rb.isKinematic = false;
+            StartCoroutine(Release());
+            Destroy(slingCircle);
+    }
+
+    private IEnumerator Release()
+    {
+        yield return new WaitForSeconds(releaseDelay);
+        sj.enabled = false;
+        //rb.position = replica.position;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        onCollision = true;
+        rb.velocity = new Vector2(0,0);
+        slingCircle = Instantiate(sling);
+        slingCircle.transform.position = transform.position;
+        StopCoroutine(Release());
+        sj.enabled = true;
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        onCollision = false;
+    }
+}
