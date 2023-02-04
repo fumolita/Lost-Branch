9b328e6b7389fec08893bb63178cd63e19ebc282
Melii <melihmilena@gmail.com>
2023-02-04 16:16:11 +0300

    ölüm

---
+++ BeikozJAM/Assets/Scripts/DeathScreen2.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathScreen2 : MonoBehaviour
+{
+   public void Dead2()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
