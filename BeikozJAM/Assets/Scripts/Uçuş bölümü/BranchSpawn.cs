679ac8560386250960799b15aca591b6bb82bf28
Melii <melihmilena@gmail.com>
2023-02-04 22:05:13 +0300

    a

--- BeikozJAM/Assets/Scripts/Uçuş bölümü/BranchSpawn.cs
+++ BeikozJAM/Assets/Scripts/Uçuş bölümü/BranchSpawn.cs
@@ -16,7 +16,7 @@ public class BranchSpawn : MonoBehaviour
     private int randomSide;
     private float randomLength;
 
-    // Start is called before the first frame update
+
     void Start()
     {
         StartCoroutine(SpawnBranches());
@@ -26,26 +26,38 @@ public class BranchSpawn : MonoBehaviour
     {
         while (true)
         {
-        yield return new WaitForSeconds(Random.Range(1, 2));
+            yield return new WaitForSeconds(Random.Range(1, 4));
 
             randomIndex = Random.Range(0, branchReference.Length);
+
+            GameObject spawnedBranch1 = Instantiate(branchReference[randomIndex]);
             randomSide = Random.Range(0, 2);
-            randomLength = Random.Range(0.5f, 4f);
-
-            spawnedBranch = Instantiate(branchReference[randomIndex]);
-            spawnedBranch = Instantiate(branchReference[randomIndex]);
-
+            randomLength = Random.Range(0.9f, 4f);
             if (randomSide == 0)
-        {
-            spawnedBranch.transform.position = leftPos.position;
-            spawnedBranch.transform.localScale = new Vector3(randomLength, 1f, 1f);
-
+            {
+                spawnedBranch1.transform.position = leftPos.position;
+                spawnedBranch1.transform.localScale = new Vector3(randomLength, 1f, 1f);
             }
-        else if (randomSide == 1)
-        {
-            spawnedBranch.transform.position = rightPos.position;
-            spawnedBranch.transform.localScale = new Vector3(-randomLength, 1f, 1f);
+            else if (randomSide == 1)
+            {
+                spawnedBranch1.transform.position = rightPos.position;
+                spawnedBranch1.transform.localScale = new Vector3(-randomLength, 1f, 1f);
+            }
+
+            GameObject spawnedBranch2 = Instantiate(branchReference[randomIndex]);
+            randomSide = Random.Range(0, 2);
+            randomLength = Random.Range(0.9f, 4f);
+            if (randomSide == 0)
+            {
+                spawnedBranch2.transform.position = leftPos.position;
+                spawnedBranch2.transform.localScale = new Vector3(randomLength, 1f, 1f);
+            }
+            else if (randomSide == 1)
+            {
+                spawnedBranch2.transform.position = rightPos.position;
+                spawnedBranch2.transform.localScale = new Vector3(-randomLength, 1f, 1f);
+            }
         }
-    }
+
     }
 }
